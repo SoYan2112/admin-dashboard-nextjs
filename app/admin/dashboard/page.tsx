d06a290aff2a45399ec40013eb226c7157c742ca
vincentyo2112 <YoTS.CE190435@gmail.com>
2026-01-12 14:32:53 +0700

    feat: dashboard stats cards using shadcn ui

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,9 +1,32 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 export default function DashboardPage() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold">Dashboard</h1>
-            <p className="text-gray-600 mt-4">Welcome to admin dashboard</p>
-            
-        </div>
-    )
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6" >
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Users</CardTitle>
+          </CardHeader>
+          <CardContent className="text-3xl font-bold">120 </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Total Orders</CardTitle>
+          </CardHeader>
+          <CardContent className="text-3xl font-bold">58</CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Revuene</CardTitle>
+          </CardHeader>
+          <CardContent className="text-3xl font-bold">2,430</CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
