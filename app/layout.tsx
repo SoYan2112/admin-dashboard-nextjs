df94f0f0dd27bf5a1d2dc5f4bf88fa858f287bd5
vincentyo2112 <YoTS.CE190435@gmail.com>
2026-01-26 11:26:29 +0700

    fix: auto redirect to dashboard on start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AdminSidebar } from "@/components/admin-sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        
+           <div className="flex">
+            <AdminSidebar/>
+            <main className="flex-1 p-6">{children}</main>
+           </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Button>Admin Dashboard</Button>
+      <Link href="/admin/dashboard" >
+      <Button variant={"default"}>
+        Home page, click to redirect to dashboard
+      </Button>
+        </Link>
     </main>
   );
 }
